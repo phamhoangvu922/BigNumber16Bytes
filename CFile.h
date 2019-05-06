e97fb889a10205fdda73ad1b03f12a6ec9913620
Pham Hoang Vu <37805526+phamhoangvu922@users.noreply.github.com>
2019-05-06 09:27:38 +0700

    Update

--- CFile.h
+++
@@ -1,9 +0,0 @@
-﻿#include"CConvert.h"
-#include"CQInt.h"
-
-const string Input = "Input.txt";
-const string Output = "Output.txt";
-// Hàm đọc file
-void ReadFile(const string Input,const string Output);
-int Find_p(string &s);
-void XL_String(string &s);
